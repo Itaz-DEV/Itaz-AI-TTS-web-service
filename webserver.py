5b8feafe576abc04ab10d82b317ae411f13e71eb
tdplaza <thien@locslab.com>
2020-08-26 14:31:54 +0900

    update no python application found problem

--- webserver.py
+++ webserver.py
@@ -1,10 +1,9 @@
 from waitress import serve
-import app
+from app import app as application
 import os
 # set current working directory to source code directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 print("\nCurrent working directory: ---'{}'---\n".format(os.getcwd()))
-application = app.app
 
 if __name__ == '__main__':
    # serve(app.app, host='0.0.0.0')
